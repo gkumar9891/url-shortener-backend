51595984d95cbd0b985a0477b714e7dfac90c960
gkumar9891 <gauravkumargk9892@gmail.com>
2024-09-21 09:01:01 +0000

    added mysql connectivity configuration

--- src/db.ts
+++ src/db.ts
@@ -7,7 +7,7 @@ const dbPassword:string = process.env.APP_DB_PASSWORD! as string;
 const dbPort:string = process.env.APP_DB_PORT! as string;
 
 const sequelize = new Sequelize(dbName, dbUserName, dbPassword, {
-  host: 'localhost',
+  host: 'mysql',
   port: parseInt(dbPort),
   dialect: 'mysql', // Dialect for MySQL
 });
