980f399c0cec24140d919075e3945cd5db35ecd7
gkumar9891 <gauravkumargk9892@gmail.com>
2024-10-08 18:06:50 +0000

    added more configuration to connect database

--- src/db.ts
+++ src/db.ts
@@ -6,11 +6,25 @@ const dbUserName:string = process.env.APP_DB_USERNAME! as string;
 const dbPassword:string = process.env.APP_DB_PASSWORD! as string;
 const dbPort:string = process.env.APP_DB_PORT! as string;
 const dbHost:string = process.env.APP_DB_HOST! as string;
+const dbUrl:string = process.env.APP_DB_URL! as string;
 
-const sequelize = new Sequelize(dbName, dbUserName, dbPassword, {
-  host: dbHost,
-  port: parseInt(dbPort),
-  dialect: 'mysql', // Dialect for MySQL
-});
+// const sequelize = new Sequelize(dbName, dbUserName, dbPassword, {
+//   host: dbHost,
+//   port: parseInt(dbPort),
+//   dialect: 'postgres', // Dialect for MySQL
+// });
+
+let sequelize: Sequelize;
+
+if(dbUrl) {
+   sequelize = new Sequelize(dbUrl);
+} else {
+  sequelize = new Sequelize(dbName, dbUserName, dbPassword, {
+    host: dbHost,
+    port: parseInt(dbPort),
+    dialect: 'mysql', // Dialect for MySQL
+  });
+}
+
 
 export default sequelize;
